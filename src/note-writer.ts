c1ceac2d1516a8e138b30c7ebefd67027bb4a2a5
Giovanni Heilmann <giovanni.heilmann@gmail.com>
2018-05-14 01:43:36 +0200

    Clean up
    Removed debug code
    Added explanatory comment for DataValidation
    Renamed function - more fitting

--- src/note-writer.ts
+++ src/note-writer.ts
@@ -9,7 +9,6 @@ export class NoteWriter {
 
   updateNotes(range, forceUpdate: boolean = false): void {
     if (range == null) return;
-    SpreadsheetApp.getActiveSpreadsheet().toast('hi');
     if (!(forceUpdate || this.docPropService.get(AutoNoteProperty))) return;
     var overwriteNote = this.docPropService.get(OverwriteNoteProperty);
     var height = range.getHeight();
@@ -19,15 +18,23 @@ export class NoteWriter {
         var cell = range.getCell(y, x);
         if (!overwriteNote && cell.getNote() != '') continue;
 
+        /* Please note:
+         * Note acquisation is done via DataValidation - is a hack, but it works.
+         * In the future, if a better solution is found, this should be replaced.
+         * How it works:
+         * The cell content is compared to validation criteria values and the note
+         * is taken from one column to the right of the corresponding criteria cell
+        */
+
         var validation = cell.getDataValidation();
-        if (this.hasRightValidationType(validation)) {
+        if (this.hasRightCriteriaType(validation)) {
           this.updateNoteFromRange(cell, validation.getCriteriaValues()[0]);
         }
       }
     }
   }
 
-  private hasRightValidationType(valid: any): boolean {
+  private hasRightCriteriaType(valid: any): boolean {
     return (
       valid != null &&
       valid.getCriteriaType() ==
@@ -41,7 +48,7 @@ export class NoteWriter {
     for (var y = 1; y <= height; ++y) {
       var currentCell = range.getCell(y, 1);
       if (currentCell.getValue() == targetValue) {
-        // assumes corresponding note content is one cell to the right of actual
+        // assumes corresponding note content is one cell to the right of currentCell
         cell.setNote(currentCell.offset(0, 1).getValue());
         return;
       }
